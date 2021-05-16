545b8412960dfab40b1e976b8fe9914bcae82c19
Besho R Mansour <beshoy.r.mansour@gmail.com>
2021-05-16 12:49:51 +0200

    Fix CORS

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -23,8 +23,15 @@ axios.interceptors.request.use(
         //   return config;
         // }
         // eslint-disable-next-line no-param-reassign
-        config.headers.Authorization = `bearer ${localStorage.getItem('token')}`;
-        config.headers.withCredentials = `true`;
+        // config.headers.Authorization = `bearer ${localStorage.getItem('token')}`;
+        // config.headers.withCredentials = `true`;
+        
+        // config.headers["Access-Control-Allow-Origin"] = "*";
+        // config.headers['Vary'] = "Origin";
+        // config.headers['Access-Control-Allow-Methods'] = "GET, POST, PUT, DELETE, HEAD, OPTIONS";
+        // config.headers["Access-Control-Allow-Credentials"] = "false";
+        // config.headers["Access-Control-Allow-Headers"] = "Content-Type accept";
+
         return config;
     },
     error => {
@@ -45,5 +52,15 @@ axios.interceptors.response.use(
         return Promise.reject(error);
     },
 );
-
+// axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+// OPTIONS http://myaccount.blob.core.windows.net/mycontainer/myblockblob  HTTP/1.1  
+// Accept: */*  
+// Origin: www.contoso.com
+// axios.defaults.headers['Access-Control-Request-Method'] = ' GET';
+// axios.defaults.headers['Origin'] = 'https://netf-test.herokuapp.com';
+// axios.defaults.headers['Access-Control-Request-Headers'] = ' content-type, accept';
+// axios.defaults.headers['Accept-Encoding'] = ' gzip, deflate  ';
+// axios.defaults.headers['User-Agent'] = ' Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)';
+// axios.defaults.headers['Content-Length'] = ' 0';
 export default axios;
--- src/redux/actions/productsActions.ts
+++ src/redux/actions/productsActions.ts
@@ -3,7 +3,17 @@ import { IGetSingleOrderItemsProductsReq, TProduct } from "../../models/Products
 import END_POINTS from '../../api/endPoints';
 import { UI_FROM_MODE } from "../../models/configs";
 
-
+// axios.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+// axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+// // OPTIONS http://myaccount.blob.core.windows.net/mycontainer/myblockblob  HTTP/1.1  
+// // Accept: */*  
+// // Origin: www.contoso.com
+// // axios.defaults.headers['Access-Control-Request-Method'] = ' GET';
+// axios.defaults.headers['Origin'] = 'http://momentum21.surge.sh';
+// axios.defaults.headers['Access-Control-Request-Headers'] = ' content-type, accept';
+// axios.defaults.headers['Accept-Encoding'] = ' gzip, deflate  ';
+// axios.defaults.headers['User-Agent'] = ' Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; WOW64; Trident/6.0)';
+// axios.defaults.headers['Content-Length'] = ' 0';
 export const getSingleOrderItemsProducts: IGetSingleOrderItemsProductsReq = (AvailableForPurchaseOnly, PageNumber, PageSize, OrderBy) => {
     return axios.get(END_POINTS.SINGLE_ORDER_ITEMS)
 }
